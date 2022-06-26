b48ffc4f683f872dd0c3d4e5fd99f00ba4f8f3f7
renzoxd245 <69137794+renzoxd245@users.noreply.github.com>
2022-06-26 15:26:48 -0500

    asdsa

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,69 +2,6 @@
 const bcryptjs = require('bcryptjs')
 const conexion = require('../database/db')
 
-//procedure to save
-<<<<<<< HEAD
-exports.saveUser= async(req, res) => {
-=======
-
-exports.saveUser_renzo = async(req, res) => {
->>>>>>> 2e61831470b18ac19dbd08ad7a2aeb660c61f9e3
-    const dni_persona = req.body.dni_persona
-    const pass = req.body.pass
-    const cpass = req.body.cpass
-    const codigo_rol = req.body.codigo_rol
-    const estado_usuario = req.body.estado_usuario
-
-    let passHash = await bcryptjs.hash(pass, 10)
-
-    conexion.query('SELECT nombre_persona,apellido_paterno,apellido_materno FROM personas WHERE dni_persona = ?', [dni_persona], (err, results, fields) => {
-        if (err) {
-            confirm.log(err);
-        }
-        if (results.length > 0) {
-            return res.render("createUser", {
-                alert: true,
-                alertMessage: 'Este Usuario(DNI) ya existe'
-            });
-        } else if (pass !== cpass) {
-            return res.render("createUser", { alert: true,
-                alertMessage: 'Las contraseñas no coinciden' })
-        }
-            data1 = JSON.stringify(results[0].nombre_persona);
-            data2 = JSON.stringify(results[0].apellido_paterno);
-            data3 = JSON.stringify(results[0].apellido_materno);
-
-            let result1 = data1.substring(2, 1);
-            const result2 = data2.slice(1, -1)
-            console.log('apellido_paterno recortado:' + result2);
-            let result3 = data3.substring(2, 1);
-
-            const nombre_usuario_mayusculas = result1.concat(result2, result3);
-            var nombre_usuario = nombre_usuario_mayusculas.toLowerCase();
-            console.log('USUARIO A CREAR:' + nombre_usuario);
-
-            //INSERTAMOS AL NUEVO USUARIO
-
-            //console.log(name + " - " + email + " - " + passHash)
-            conexion.query('INSERT INTO usuarios SET ?', {dni_persona: dni_persona,codigo_rol: Number(codigo_rol) ,nombre_usuario:nombre_usuario, pass: passHash, estado_usuario: "Activo"}, (error, results) => {
-            if(error) {
-            console.error(error)
-            res.render('register', {
-                alert: true,
-                alertMessage: 'Este usuario ya existe'
-            })
-            } else {   
-            res.redirect('/')
-         }
-        }); 
-});
-};
-
-
-
-
-
-
 //exports.searchuser = (req, res) => {
 exports.saveUser = (req, res) => {
     //var action = req.body.action;
