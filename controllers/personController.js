d2e1deb72dc2a261b2ec92012dc082d2954cdaa3
renzoxd245 <69137794+renzoxd245@users.noreply.github.com>
2022-06-26 13:17:55 -0500

    CAMBIOSULTIMOS

---
+++ controllers/personController.js
@@ -0,0 +1,74 @@
+//invoke the DB connection
+const bcryptjs = require('bcryptjs')
+const conexion = require('../database/db')
+
+//procedure to save
+exports.savePerson = async(req, res) => {
+    const dni_persona = req.body.dni_persona
+    const pass = req.body.pass
+    const cpass = req.body.cpass
+    const codigo_rol = req.body.codigo_rol
+    const estado_usuario = req.body.estado_usuario
+
+    let passHash = await bcryptjs.hash(pass, 10)
+
+    conexion.query('SELECT nombre_persona,apellido_paterno,apellido_materno FROM personas WHERE dni_persona = ?', [dni_persona], (err, results, fields) => {
+        if (err) {
+            confirm.log(err);
+        }
+        if (results.length > 0) {
+            return res.render("createUser", {
+                alert: true,
+                alertMessage: 'Este Usuario(DNI) ya existe'
+            });
+        } else if (pass !== cpass) {
+            return res.render("createUser", { alert: true,
+                alertMessage: 'Las contraseñas no coinciden' })
+        }
+            data1 = JSON.stringify(results[0].nombre_persona);
+            data2 = JSON.stringify(results[0].apellido_paterno);
+            data3 = JSON.stringify(results[0].apellido_materno);
+
+            let result1 = data1.substring(2, 1);
+            const result2 = data2.slice(1, -1)
+            console.log('apellido_paterno recortado:' + result2);
+            let result3 = data3.substring(2, 1);
+
+            const nombre_usuario_mayusculas = result1.concat(result2, result3);
+            var nombre_usuario = nombre_usuario_mayusculas.toLowerCase();
+            console.log('USUARIO A CREAR:' + nombre_usuario);
+
+            //INSERTAMOS AL NUEVO USUARIO
+
+            //console.log(name + " - " + email + " - " + passHash)
+            conexion.query('INSERT INTO usuarios SET ?', {dni_persona: dni_persona,codigo_rol: Number(codigo_rol) ,nombre_usuario:nombre_usuario, pass: passHash, estado_usuario: "Activo"}, (error, results) => {
+            if(error) {
+            console.error(error)
+            res.render('register', {
+                alert: true,
+                alertMessage: 'Este usuario ya existe'
+            })
+            } else {   
+            res.redirect('/')
+         }
+        }); 
+});
+};
+
+//procedure to update
+exports.updatePerson =  async(req, res) => {
+    const usuario = req.body.usuario
+    const dni_persona = req.body.dni_persona
+    const pass = req.body.pass
+    const codigo_rol = req.body.codigo_rol
+    const estado_usuario = req.body.estado_usuario
+
+    let passHash = await bcryptjs.hash(pass, 10)
+    conexion.query('UPDATE usuarios SET ? WHERE usuario = ?', [{ dni_persona:dni_persona, pass:passHash, codigo_rol:codigo_rol, estado_usuario:estado_usuario}, usuario ], (error, results) => {
+        if(error) {
+            console.error(error)
+        } else {
+            res.redirect('/');
+        }
+    })
+}
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 //to invoke the methods for the CRUD of users
 const userController = require('../controllers/userController')
+const personController = require('../controllers/personController')
 const authController = require('../controllers/authController')
 const { Router } = require('express')
 
@@ -70,6 +71,64 @@ router.get('/deleteUser/:usuario', (req, res) => {
 
 router.post('/saveUser', userController.saveUser)
 router.post('/updateUser', userController.updateUser)
+
+//PERSONAS RUTAS Y FRONT
+router.get('/persons', authController.isAuthenticated, (req, res) => {
+    // res.send('hola mundo')    
+    conexion.query('SELECT * FROM personas', (error, results) => {
+        if(error){
+            throw error;
+        } else {
+            // res.send(results);
+            if (row.codigo_rol=="1") { 
+                res.render('persons', { results: results, titleWeb: "List persons" })
+            } else {
+                res.render('index', { userName: row.name, image: row.image, titleWeb: "Control Dashboard"})
+            }
+        }
+    })
+})
+
+//path to create a person record
+router.get('/createPerson', authController.isAuthenticated, (req, res) => {
+    if (row.codigo_rol=="1") {        
+        res.render('createPerson', { titleWeb: "Create person"})
+    } else {
+        res.render('index', { userName: row.name, image: row.image, titleWeb: "Control Dashboard"})
+    }
+})
+
+//path to edit a selected person record
+router.get('/editPerson/:dni_persona', authController.isAuthenticated, (req, res) => {
+    const dni_persona = req.params.dni_persona;
+    conexion.query('SELECT * FROM personas WHERE dni_persona= ?', [dni_persona], (error, results) => {
+        if(error){
+            throw error;
+        } else {
+            if(row.codigo_rol=="1") {
+                res.render('editPerson', { user: results[0], titleWeb: "Edit person" })
+            } else {
+                res.render('index', { userName: row.name, image: row.image, titleWeb: "Control Dashboard"})
+            }
+        }
+    })
+})
+
+//path to delete a selected person record
+router.get('/deleteUser/:dni_persona', (req, res) => {
+    const usuario = req.params.usuario
+    conexion.query('DELETE FROM personas WHERE dni_persona= ?', [dni_persona], (error, results) => {
+        if(error){
+            throw error;
+        } else {
+            res.redirect('/persons')
+        }
+    })
+});
+
+router.post('/savePerson', personController.savePerson)
+router.post('/updatePerson', personController.updatePerson)
+
 
 
 //router for views
